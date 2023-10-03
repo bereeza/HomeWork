599de609ea73a357724fe90f2e40e60123ecc835
Timofii <timofejberezanskij@gmail.com>
2023-10-03 12:51:33 +0300

    task 11

--- src/main/java/org/example/lesson15/Main.java
+++ src/main/java/org/example/lesson15/Main.java
@@ -5,5 +5,9 @@ public class Main {
         ValueCalculator calculator = new ValueCalculator();
         // Time: 35ms
         calculator.calculateTime();
+
+        // Also, noticed:
+        // if i delete thread in divideArrayIntoTwo time is 53
+        // if i use thread time is <40
     }
 }
