af765070d9d1e4b37f1287c7e675cf503bc69a6e
Timofii <timofejberezanskij@gmail.com>
2023-10-08 13:06:25 +0300

    task 16

--- src/main/java/org/example/lesson16/task2/PetrolStation.java
+++ src/main/java/org/example/lesson16/task2/PetrolStation.java
@@ -2,28 +2,25 @@ package org.example.lesson16.task2;
 
 import lombok.*;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Getter
 @Setter
-public class PetrolStation implements RefuelingProcess{
-    private int amount;
-    private final int MAX_CARS_AMOUNT = 3;
-    private int cars = 0;
+public class PetrolStation implements RefuelingProcess {
+    private volatile AtomicInteger amount;
 
     public PetrolStation(int amount) {
-        this.amount = amount;
+        this.amount = new AtomicInteger(amount);
     }
 
     @SneakyThrows
-    public synchronized void doRefuel(int fuelAmount) {
-        if (amount < fuelAmount) {
+    public void doRefuel(int fuelAmount) {
+        if (amount.get() < fuelAmount) {
             throw new FuelIsOutException("There is no or not enough fuel");
         }
-        cars++;
         System.out.println("Refueling started.");
-        amount -= fuelAmount;
+        amount.updateAndGet(amount -> amount - fuelAmount);
         Thread.sleep(RefuelingProcess.timeOfRefuel());
         System.out.println("Refueling completed for Car. Fuel total: " + amount);
-        cars--;
-        notifyAll();
     }
 }
