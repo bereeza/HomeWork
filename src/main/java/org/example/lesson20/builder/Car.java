2416aa42ad8209e390e8c26d33b446440b731f23
Timofii <timofejberezanskij@gmail.com>
2023-10-31 17:36:23 +0200

    task 20 & abstract factory & builder

--- src/main/java/org/example/lesson20/builder/Car.java
+++ src/main/java/org/example/lesson20/builder/Car.java
@@ -1,17 +1,59 @@
 package org.example.lesson20.builder;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
-import org.example.lesson20.builder.carType.CarType;
+import org.example.lesson20.builder.car_type.Type;
 
 @Getter
-@Setter
-@AllArgsConstructor
 @ToString
 public class Car {
     private String engine;
-    private CarType type;
     private int wheels;
+    private int horsePower;
+    private Type type;
+
+    public Car(Builder builder) {
+        this.engine = builder.engine;
+        this.wheels = builder.wheels;
+        this.horsePower = builder.horsePower;
+        this.type = builder.type;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    static final class Builder{
+        private String engine;
+        private int wheels;
+        private int horsePower;
+        private Type type;
+        private Builder() {
+
+        }
+
+        public Builder engine(String engine) {
+            this.engine = engine;
+            return this;
+        }
+
+        public Builder wheels(int wheels) {
+            this.wheels = wheels;
+            return this;
+        }
+
+        public Builder horsePower(int horsePower) {
+            this.horsePower = horsePower;
+            return this;
+        }
+
+        public Builder type(Type type) {
+            this.type = type;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
+    }
 }
--- src/main/java/org/example/lesson20/builder/Main.java
+++ src/main/java/org/example/lesson20/builder/Main.java
@@ -1,16 +1,11 @@
 package org.example.lesson20.builder;
 
-import org.example.lesson20.builder.carType.CarType;
+import org.example.lesson20.builder.car_type.Type;
 
 public class Main {
     public static void main(String[] args) {
-        Builder carBuilder = new CarBuilder();
-        Director director = new Director(carBuilder);
-
-        director.constructCar("Porsche 911 GT3 4.0 NA F6", CarType.SPORT_CAR, 4);
-        Car car = director.getResult();
-
-        // A car has been created: Car(engine=Porsche 911 GT3 4.0 NA F6, type=SPORT_CAR, wheels=4)
-        System.out.println("A car has been created: " + car);
+        Car car = Car.builder().engine("McLaren MP4-12C 3.8 TT").wheels(4).horsePower(450).type(Type.SPORT_CAR).build();
+        // Car(engine=McLaren MP4-12C 3.8 TT, wheels=4, horsePower=450, type=SPORT_CAR)
+        System.out.println(car);
     }
 }
